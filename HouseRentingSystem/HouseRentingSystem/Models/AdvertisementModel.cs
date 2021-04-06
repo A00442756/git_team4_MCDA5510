fc463cac893f3e305e4bcc82c25a62e88405d06f
A00445457 <wenshuo.li@smu.ca>
2021-04-06 16:06:55 +0800

    add require in advertisement model

--- HouseRentingSystem/HouseRentingSystem/Models/AdvertisementModel.cs
+++ HouseRentingSystem/HouseRentingSystem/Models/AdvertisementModel.cs
@@ -17,9 +17,11 @@ namespace HouseRentingSystem.Models
         public int Userid { get; set; }
         public bool Ondisplay { get; set; }
         [StringLength(100, MinimumLength = 5)]
-        [Required(ErrorMessage = "Please enter the title of your book")]
+        [Required(ErrorMessage = "Please enter the title of your advertisement")]
         public string Title { get; set; }
         public int Rental { get; set; }
+        [Display(Name = "write some description about your house/apartment")]
+        [Required]
         public string Description { get; set; }
         public string Country { get; set; }
         public string Province { get; set; }
@@ -28,20 +30,40 @@ namespace HouseRentingSystem.Models
         public string Streetnum { get; set; }
         public int Bedroomsnum { get; set; }
         public int Bathroomsnum { get; set; }
+        [Display(Name = "Choose if your house/apartment provide hydro ")]
+        [Required]
         public bool Hydro { get; set; }
+        [Display(Name = "Choose if your house/apartment provide heat ")]
+        [Required]
         public bool Heat { get; set; }
+        [Display(Name = "Choose if your house/apartment provide water ")]
+        [Required]
         public bool Water { get; set; }
+        [Display(Name = "Choose if your house/apartment provide Internet ")]
+        [Required]
         public bool Internet { get; set; }
         public int Parkingnum { get; set; }
         public string Agreementtype { get; set; }
         public DateTime Moveindate { get; set; }
+        [Display(Name = "Choose if your house/apartment permits pets")]
+        [Required]
         public bool Petfriendly { get; set; }
         public int Size { get; set; }
         public bool Furnished { get; set; }
+        [Display(Name = "Choose if your house/apartment provide laundry ")]
+        [Required]
         public bool Laundry { get; set; }
+        [Display(Name = "Choose if your house/apartment provide dishwasher ")]
+        [Required]
         public bool Dishwasher { get; set; }
+        [Display(Name = "Choose if your house/apartment provide fridge ")]
+        [Required]
         public bool Fridge { get; set; }
+        [Display(Name = "Choose if your house/apartment provide air-condition ")]
+        [Required]
         public bool Airconditioning { get; set; }
+        [Display(Name = "Choose if your house/apartment permits smoking")]
+        [Required]
         public bool Smokingpermit { get; set; }
         public DateTime Postdate { get; set; }
 
