4244b1fd89d07f99a2593adc8054e8460d66e492
A00445457 <wenshuo.li@smu.ca>
2021-04-08 18:23:00 +0800

    add 2 col posatalcode and phonenum on advertisement

--- HouseRentingSystem/HouseRentingSystem/Controllers/AdvertisementController.cs
+++ HouseRentingSystem/HouseRentingSystem/Controllers/AdvertisementController.cs
@@ -37,9 +37,10 @@ namespace HouseRentingSystem.Controllers
             return View(advertisement);
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Editad(int Adid, [Bind("Adid,Userid,Ondisplay,Title,Rental,Description,Country,Province,City,Streetname,Streetnum,Bedroomsnum,Bathroomsnum,Hydro,Heat,Water,Internet,Parkingnum,Agreementtype,Moveindate,Petfriendly,Size,Furnished,Laundry,Dishwasher,Fridge,Airconditioning,Smokingpermit")] AdvertisementModel advertisement)
+        public async Task<IActionResult> Editad(int Adid, [Bind("Adid,Userid,Ondisplay,Title,Rental,PostalCode,ContactPhoneNum,Description,Country,Province,City,Streetname,Streetnum,Bedroomsnum,Bathroomsnum,Hydro,Heat,Water,Internet,Parkingnum,Agreementtype,Moveindate,Petfriendly,Size,Furnished,Laundry,Dishwasher,Fridge,Airconditioning,Smokingpermit")] AdvertisementModel advertisement)
         {
             if (Adid != advertisement.Adid)
             {
--- HouseRentingSystem/HouseRentingSystem/Migrations/20210408101802_add2colpostalandphone.Designer.cs
+++ HouseRentingSystem/HouseRentingSystem/Migrations/20210408101802_add2colpostalandphone.Designer.cs
@@ -4,14 +4,16 @@ using HouseRentingSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HouseRentingSystem.Migrations
 {
     [DbContext(typeof(HouseRentingSystemDBContext))]
-    partial class HouseRentingSystemDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210408101802_add2colpostalandphone")]
+    partial class add2colpostalandphone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
